c99bfa042d0c26b6252afd33bc7fd87e4343440b
jaekyun.Ahn <ajg10795@naver.com>
2024-08-28 02:11:58 +0900

    HelloWorld 폴더 정리

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,11 @@
+//  Java code : 일반적인 Java Code 실행 방법은 java file을 Build하여 Class file 생성 후 실행
+//  VScode에서는 간단하게 Test를 할 수 있지만 실제 사용해먹으려면 cmd에서 Build하여 사용하는게 바람직
+//      build command : javac <class명>.java
+//      run command : java <class명>
+
+public class HelloWorld{
+    //  run 누르면 아래 function이 실행됨.
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
